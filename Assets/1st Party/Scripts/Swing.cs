36a3b9a9a09fa9577264fb8aeb45e0ae4f946e78
jhutch52 <59901555+jhutch52@users.noreply.github.com>
2021-03-25 17:13:33 -0400

    begun sword implementation and animations, file structure rework

---
+++ Assets/1st Party/Scripts/Swing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swing : MonoBehaviour
+{
+
+    private bool swingDown;
+
+    public LayerMask enemy;
+
+    public Animator swordAnimator;
+    public Animator gunAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        swingDown = Input.GetButtonDown("Fire2");
+        if (swingDown)
+        {
+            swordAnimator.SetTrigger("Swing");
+            gunAnimator.SetTrigger("Swing");
+        }
+    }
+}
